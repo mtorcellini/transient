2409ea8c9120beeeb4c10082d422f17e8f47090f
actuallyDan <dankral01@gmail.com>
2020-10-23 05:16:17 -0400

    Updates

---
+++ precache-manifest.d4c23a07d5d7f93e0d3c677ad1e9944f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b793277e63964b94ac78630cb9637d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91a5c3680268a7f87ad5",
+    "url": "/static/css/main.03880762.chunk.css"
+  },
+  {
+    "revision": "3fe1914727aaf2e3b26f",
+    "url": "/static/js/2.f30dca90.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f30dca90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a5c3680268a7f87ad5",
+    "url": "/static/js/main.bd3ab2f4.chunk.js"
+  },
+  {
+    "revision": "4b9620f2155a5545632a",
+    "url": "/static/js/runtime-main.46802875.js"
+  }
+]);
