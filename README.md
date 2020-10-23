c964905b4062a2e0b6260f9674871b6f2df197c1
actuallyDan <dankral01@gmail.com>
2020-10-23 05:00:41 -0400

    minor tweak and added gh-pages

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-# [Transient](https://transient-composer.herokuapp.com)
+# [Transient](https://actuallydan.github.io/transient-composer)
+
 ## Procedurally Composed Machine Music
-----
 
-#### Using [Pizzicato.js](https://github.com/alemangui/pizzicato), Transient composes and plays programmatically "okay" music - indefinitely.
+---
 
-* Transient uses four part voice writing, along with a "kick" and a "clap" to create music that winds randomly but never out of key or out of time
+#### Using [Pizzicato.js](https://github.com/alemangui/pizzicato), Transient composes and plays programmatically "okay" "music"(?) - indefinitely.
 
-* Transient plays at 60 bpm (or one beat per second), and uses C Major for simplicity, although the ability to use different scales and even modulation is possible with little change.
+- Transient uses four part voice writing, along with a "kick" and a "clap" to create music that winds randomly but never out of key or out of time
 
-* Notes in each voice are composed "just in time" meaning they are not predetermined, but instead are composed just before they are about to be played, along with the duration of the note.
+- Transient plays at 60 bpm (or one beat per second), and uses C Major for simplicity, although the ability to use different scales and even modulation is possible with little change.
 
-* At this time, subsequent notes and duration are determined randomly within a given range but there is potential for some basic machine learning to help create musical patterns and better-follow basic music theory.
+- Notes in each voice are composed "just in time" meaning they are not predetermined, but instead are composed just before they are about to be played, along with the duration of the note.
 
-* There are also plans to allow user interaction to change values such as key, the range of octaves availabe to each voice, custom keys, variable tempo and more. 
+- At this time, subsequent notes and duration are determined randomly within a given range but there is potential for some basic machine learning to help create musical patterns and better-follow basic music theory.
 
-* In protoyping, I created a means for the notes to be displayed on the page as they were played, but I removed them for now until I could come up with a way for it to be saved or exportable into a useable music file format.
+- There are also plans to allow user interaction to change values such as key, the range of octaves availabe to each voice, custom keys, variable tempo and more.
+
+- In protoyping, I created a means for the notes to be displayed on the page as they were played, but I removed them for now until I could come up with a way for it to be saved or exportable into a useable music file format.
--- src/App.js
+++ src/App.js
@@ -1,52 +1,64 @@
-import React, { Component } from 'react';
-import './App.css';
-import Player from './Player.js';
+import React, { Component } from "react";
+import "./App.css";
+import Player from "./Player.js";
 
-import ColorChanger from './ColorChanger.js';
+import ColorChanger from "./ColorChanger.js";
 
 const changer = new ColorChanger();
 
 export default class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       playToggle: false,
-      player: new Player()
+      player: new Player(),
     };
-
   }
-  togglePlay(){
-    this.setState({
-      playToggle : !this.state.playToggle
-    }, ()=>{
-      this.state.player.playMusic(this.state.playToggle);
-      this.state.playToggle ? changer.startTransition() : changer.stopTransition();
-    });
+  togglePlay() {
+    this.setState(
+      {
+        playToggle: !this.state.playToggle,
+      },
+      () => {
+        this.state.player.playMusic(this.state.playToggle);
+        this.state.playToggle
+          ? changer.startTransition()
+          : changer.stopTransition();
+      }
+    );
   }
   render() {
     return (
       <div className="App">
-        <div id="header">Transient composes and plays programmatically "okay" music.
-        <br/>
-         Indefinitely.</div>
-        <button id="play" onClick={this.togglePlay.bind(this)}>{this.state.playToggle ? "Stop" : "Play"}</button>
-        <div id="credit">Procedurally Composed Machine Music <br/> Built with 
+        <div id="header">
+          Transient composes and plays programmatically "okay" music.
+          <br />
+          Indefinitely.
+          <div style={{ fontFamily: "monospace", marginTop: "1em" }}>
+            You may wish to lower your volume, just in case!
+          </div>
+        </div>
+        <button id="play" onClick={this.togglePlay.bind(this)}>
+          {this.state.playToggle ? "Stop" : "Play"}
+        </button>
+        <div id="credit">
+          Procedurally Composed Machine Music <br /> Built with
           <div id="logo-wrapper">
-
             <a href="https://github.com/alemangui/pizzicato">
-            <div>Pizzicato.js</div>
-            <img id="pizzi-logo" src="https://alemangui.github.io/pizzicato/favicon.png" alt="Pizzicato.js"/>
+              <div>Pizzicato.js</div>
+              <img
+                id="pizzi-logo"
+                src="https://alemangui.github.io/pizzicato/favicon.png"
+                alt="Pizzicato.js"
+              />
             </a>
             <a href="https://reactjs.org/">
-              <img id="react-logo" src="./download.svg" /> 
+              <img id="react-logo" src="./download.svg" />
               <div>React</div>
             </a>
           </div>
+        </div>
       </div>
-
-      </div>
-      );
+    );
   }
 }
-
-
